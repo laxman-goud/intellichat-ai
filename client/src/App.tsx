765fcf301a0de05c081351573771e7a55ed9123c
laxman-goud <codewithlaxman92@gmail.com>
2025-12-24 17:28:02 +0530

    client(feat): add mobile menu in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,43 @@
-import { Route, Routes } from "react-router-dom"
-import Sidebar from "./components/Sidebar"
-import ChatBox from "./components/ChatBox"
-import Credits from "./pages/Credits"
-import Community from "./pages/Community"
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
+import Sidebar from "./components/Sidebar";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
+
+import { assets } from "./assets/assets";
 
 const App = () => {
-  return (
-      <>
-        <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
-          <div className="flex h-screen w-screen">
-            <Sidebar />
-            <Routes>
-              <Route path="/" element={<ChatBox />} />
-              <Route path="/credits" element={<Credits />} />
-              <Route path="/community" element={<Community />} />
-            </Routes>
-          </div>
-        </div>
-      </>
-  )
-}
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-export default App
+  return (
+    <>
+      {!isMenuOpen && (
+        <img
+          src={assets.menu_icon}
+          onClick={() => setIsMenuOpen(true)}
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+          alt="menu"
+        />
+      )}
+
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar
+            isMenuOpen={isMenuOpen}
+            setIsMenuOpen={setIsMenuOpen}
+          />
+
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
