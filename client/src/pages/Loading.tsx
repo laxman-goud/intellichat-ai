f40ad3bf3b2572beaa6a3e8c74183d1823b0cc3d
laxman-goud <codewithlaxman92@gmail.com>
2025-12-25 20:12:45 +0530

    client(feat): enhance Loading component with styled loading animation and navigation

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,9 +1,22 @@
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Loading
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate("/");
+    }, 8000);
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
+  return (
+    <div className="bg-linear-to-b from-[#531B81] to-[#29184B] backdrop-opacity-60 flex items-center justify-center h-screen w-screen text-white text-2xl">
+      <div className="w-10 h-10 rounded-full border-3 border-white border-t-transparent animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
